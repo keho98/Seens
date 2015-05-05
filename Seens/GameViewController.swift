6a88a584a74ebdcf364eafd2475af5160dda1062
Kevin Ho <keho98@gmail.com>
2015-05-04 23:01:51 -0700

    Initial commit

--- Seens/GameViewController.swift
+++ Seens/GameViewController.swift
@@ -16,7 +16,12 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.dae")!
+        let scene = SCNScene()
+        
+        let boxGeometry = SCNBox(width: 5.0, height: 5.0, length: 5.0, chamferRadius: 1.0)
+        boxGeometry.firstMaterial!.diffuse.contents = UIColor(red: 0.1, green: 0.2, blue: 0.7, alpha: 0.9)
+        let boxNode = SCNNode(geometry: boxGeometry)
+        scene.rootNode.addChildNode(boxNode)
         
         // create and add a camera to the scene
         let cameraNode = SCNNode()
@@ -26,40 +31,26 @@ class GameViewController: UIViewController {
         // place the camera
         cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
         
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = SCNLightTypeOmni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
-        
         // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = SCNLightTypeAmbient
-        ambientLightNode.light!.color = UIColor.darkGrayColor()
-        scene.rootNode.addChildNode(ambientLightNode)
+        let spotLightNode = SCNNode()
+        spotLightNode.light = SCNLight()
+        spotLightNode.light!.type = SCNLightTypeSpot
+        spotLightNode.light!.color = UIColor(white: 0.9, alpha: 1.0)
+        spotLightNode.position = SCNVector3(x: 10, y: 10, z: 10)
+        spotLightNode.castsShadow = true
+
+        let constraint = SCNLookAtConstraint(target: boxNode)
+        spotLightNode.constraints = [constraint]
+        scene.rootNode.addChildNode(spotLightNode)
         
-        // retrieve the ship node
-        let ship = scene.rootNode.childNodeWithName("ship", recursively: true)!
-        
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatActionForever(SCNAction.rotateByX(0, y: 2, z: 0, duration: 1)))
-        
-        // retrieve the SCNView
         let scnView = self.view as! SCNView
         
-        // set the scene to the view
         scnView.scene = scene
-        
-        // allows the user to manipulate the camera
         scnView.allowsCameraControl = true
-        
-        // show statistics such as fps and timing information
         scnView.showsStatistics = true
         
         // configure the view
-        scnView.backgroundColor = UIColor.blackColor()
+        scnView.backgroundColor = UIColor.grayColor()
         
         // add a tap gesture recognizer
         let tapGesture = UITapGestureRecognizer(target: self, action: "handleTap:")
@@ -79,31 +70,7 @@ class GameViewController: UIViewController {
         let p = gestureRecognize.locationInView(scnView)
         if let hitResults = scnView.hitTest(p, options: nil) {
             // check that we clicked on at least one object
-            if hitResults.count > 0 {
-                // retrieved the first clicked object
-                let result: AnyObject! = hitResults[0]
-                
-                // get its material
-                let material = result.node!.geometry!.firstMaterial!
-                
-                // highlight it
-                SCNTransaction.begin()
-                SCNTransaction.setAnimationDuration(0.5)
-                
-                // on completion - unhighlight
-                SCNTransaction.setCompletionBlock {
-                    SCNTransaction.begin()
-                    SCNTransaction.setAnimationDuration(0.5)
-                    
-                    material.emission.contents = UIColor.blackColor()
-                    
-                    SCNTransaction.commit()
-                }
-                
-                material.emission.contents = UIColor.redColor()
-                
-                SCNTransaction.commit()
-            }
+            println(hitResults)
         }
     }
     
