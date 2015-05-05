859f8765976fa3d64274a607b13e521781895f60
Kevin Ho <keho98@gmail.com>
2015-05-04 23:43:18 -0700

    Basic animation working

--- Seens/GameViewController.swift
+++ Seens/GameViewController.swift
@@ -18,9 +18,10 @@ class GameViewController: UIViewController {
         // create a new scene
         let scene = SCNScene()
         
-        let boxGeometry = SCNBox(width: 5.0, height: 5.0, length: 5.0, chamferRadius: 1.0)
+        let boxGeometry = SCNBox(width: 4.0, height: 4.0, length: 4.0, chamferRadius: 1.0)
         boxGeometry.firstMaterial!.diffuse.contents = UIColor(red: 0.1, green: 0.2, blue: 0.7, alpha: 0.9)
         let boxNode = SCNNode(geometry: boxGeometry)
+        boxNode.position =  SCNVector3(x: 2, y: 5, z: 0)
         scene.rootNode.addChildNode(boxNode)
         
         // create and add a camera to the scene
@@ -29,7 +30,7 @@ class GameViewController: UIViewController {
         scene.rootNode.addChildNode(cameraNode)
         
         // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 20)
         
         // create and add an ambient light to the scene
         let spotLightNode = SCNNode()
@@ -52,6 +53,9 @@ class GameViewController: UIViewController {
         // configure the view
         scnView.backgroundColor = UIColor.grayColor()
         
+        // add animations
+        boxNode.runAction(SCNAction.repeatActionForever(SCNAction.rotateByX(CGFloat(0.0), y: CGFloat(0.0), z: CGFloat(2.0*M_PI), duration: 2.0)))
+        
         // add a tap gesture recognizer
         let tapGesture = UITapGestureRecognizer(target: self, action: "handleTap:")
         var gestureRecognizers = [AnyObject]()
@@ -66,7 +70,6 @@ class GameViewController: UIViewController {
         // retrieve the SCNView
         let scnView = self.view as! SCNView
         
-        // check what nodes are tapped
         let p = gestureRecognize.locationInView(scnView)
         if let hitResults = scnView.hitTest(p, options: nil) {
             // check that we clicked on at least one object
